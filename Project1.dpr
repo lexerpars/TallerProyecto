fcd4179ee162db6fcb91115c931e800f04d3d6ea
lexerpars <gersonrogelio@gmail.com>
2017-08-21 06:16:43 -0700

    [REF] avances

--- Project1.dpr
+++ Project1.dpr
@@ -3,12 +3,16 @@ program Project1;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Unit1 in 'Unit1.pas' {Form1};
+  Unit1 in 'Unit1.pas' {Form1},
+  Login in 'Login.pas' {Frame2: TFrame},
+  Datos in 'Datos.pas' {DataModule2: TDataModule},
+  PanelOrdenes in 'PanelOrdenes.pas' {Frame3: TFrame};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TDataModule2, DataModule2);
   Application.Run;
 end.
--- Unit1.pas
+++ Unit1.pas
@@ -4,22 +4,56 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.StdCtrls, FMX.Menus, Login, PanelOrdenes,FMX.Controls.Presentation, FMX.Objects;
+
+  //Uses login
 
 type
   TForm1 = class(TForm)
     ScaledLayout1: TScaledLayout;
+    Contenedor: TPanel;
+    Rectangle1: TRectangle;
+    OrdenesContenedor: TPanel;
+    Rectangle2: TRectangle;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
-    { Public declarations }
+  //Login variable que contiene Frame de Login
+    Login : TFrame2;
+    PanelOrdenes : TFrame3;
+    procedure Iniciar_ordenes();
   end;
 
 var
   Form1: TForm1;
+  Agente,Nombre,Estatus:String;
 
 implementation
 
+
 {$R *.fmx}
+
+
+procedure TForm1.Iniciar_ordenes;
+begin
+  Login.Free;
+  PanelOrdenes := TFrame3.Create(self);
+  PanelOrdenes.Parent:=Contenedor;
+  PanelOrdenes.Visible:=True;
+  PanelOrdenes.EtiquetaNombre.Text:=Nombre;
+  PanelOrdenes.carga_ordenes;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  //Cargar Frame Login
+  Login := TFrame2.Create(nil);
+  Login.Parent := Contenedor;
+  Login.Visible := True;
+
+end;
+
 
 end.
